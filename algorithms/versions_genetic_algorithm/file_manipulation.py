56b81e88350f1820ad03e08e31699c31fac912f5
Gustavo Moura <gus.moura@outlook.com>
2019-10-22 07:39:57 -0300

    melhorias no 35 e updates no 23, pronto para começar escrita

--- algorithms/versions_genetic_algorithm/file_manipulation.py
+++ algorithms/versions_genetic_algorithm/file_manipulation.py
@@ -30,6 +30,30 @@ def read_mission(missao_filename, missao_id, mapa_filename):
     return mapa
 
 
+def upload_mapa(mapa_file, mapa_id):
+    mapa = mapa_file[mapa_id]
+
+    geo_home = GeoPoint(mapa['geo_home'][1], mapa['geo_home'][0], mapa['geo_home'][2])
+
+    areas_bonificadoras  = [ Conversor.list_geo_to_cart(area['geo_points'], geo_home) for area in mapa['areas_bonificadoras'] ]
+    areas_penalizadoras  = [ Conversor.list_geo_to_cart(area['geo_points'], geo_home) for area in mapa['areas_penalizadoras'] ]
+    
+    areas_nao_navegaveis = []
+    for area in mapa['areas_nao_navegaveis']:
+        geo_points=[]
+        for geo_point in area['geo_points']:
+            geo_points.append(Conversor.geo_to_cart(
+                GeoPoint(geo_point[1], geo_point[0], geo_point[2])
+                , geo_home))
+        areas_nao_navegaveis.append(geo_points)
+
+
+
+    return geo_home, areas_bonificadoras, areas_penalizadoras, areas_nao_navegaveis
+
+
+
+
 def read_mapa(mapa_filename, mapa_id):
     
     with open(mapa_filename, 'r') as mapa_file:
