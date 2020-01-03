fc0d1c136f8390f67f7b3a68f6f3613a1332e3da
Gustavo <gus.moura@outlook.com>
2020-01-03 19:11:03 -0300

    adaptando notebook para codigo .py

--- genetic/file_manipulation.py
+++ genetic/file_manipulation.py
@@ -1,23 +1,30 @@
+from genetic.data_definitions import GeoPoint, Conversor
 
 
-def upload_map(map_file, map_id):
-    map = map_file[map_id]
+def upload_mapa(mapa_file, mapa_id):
+    mapa = mapa_file[mapa_id]
 
-    geo_home = GeoPoint(map['geo_home'][1], map['geo_home'][0], map['geo_home'][2])
+    geo_home = GeoPoint(mapa["geo_home"][1], mapa["geo_home"][0], mapa["geo_home"][2])
 
-    areas_bonificadoras  = [ Conversor.list_geo_to_cart(area['geo_points'], geo_home) for area in map['areas_bonificadoras'] ]
-    areas_penalizadoras  = [ Conversor.list_geo_to_cart(area['geo_points'], geo_home) for area in map['areas_penalizadoras'] ]
-    
+    areas_bonificadoras = [
+        Conversor.list_geo_to_cart(area["geo_points"], geo_home)
+        for area in mapa["areas_bonificadoras"]
+    ]
+    areas_penalizadoras = [
+        Conversor.list_geo_to_cart(area["geo_points"], geo_home)
+        for area in mapa["areas_penalizadoras"]
+    ]
+
     areas_nao_navegaveis = []
-    for area in map['areas_nao_navegaveis']:
-        geo_points=[]
-        for geo_point in area['geo_points']:
-            geo_points.append(Conversor.geo_to_cart(
-                GeoPoint(geo_point[1], geo_point[0], geo_point[2]) 
-                , geo_home))
+    for area in mapa["areas_nao_navegaveis"]:
+        geo_points = []
+        for geo_point in area["geo_points"]:
+            geo_points.append(
+                Conversor.geo_to_cart(
+                    GeoPoint(geo_point[1], geo_point[0], geo_point[2]), geo_home
+                )
+            )
         geo_points.append(geo_points[0])
         areas_nao_navegaveis.append(geo_points)
-
-
 
     return geo_home, areas_bonificadoras, areas_penalizadoras, areas_nao_navegaveis
