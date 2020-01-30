5907f52d0c0a50374f6ad73aa42640455fa23e5d
gustavo moura <gus.moura@outlook.com>
2020-01-30 20:34:07 -0300

    adição de alguns testes e modificações na visualização

--- genetic/visualization.py
+++ genetic/visualization.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
 
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 from sklearn import preprocessing
 
